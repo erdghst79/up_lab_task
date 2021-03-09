33221075416db8a2bfb2226e48141983bc03984d
Ihor Barmak <ihor@uplab.io>
2021-03-09 15:17:42 +0200

    feat: added db

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import LoadingOverlay from './components/common/LoadingOverlay';
 import RootRouter from './router/RootRouter';
 import startup from './startup';
 import 'normalize.css';
 import './styles/index.scss';
+import DB from './db';
 
 startup();
 
-const App = () => <RootRouter />;
+const useLoadDb = () => {
+  const [dbLoaded, setDbLoaded] = React.useState(false);
+  React.useEffect(() => {
+    const offInit = DB.onInit((result) => {
+      if (!result.success) {
+        // eslint-disable-next-line no-alert
+        alert(`Error loading app. It looks like your device is not supported. (${result.error})`);
+        console.log(result);
+      } else {
+        setDbLoaded(true);
+      }
+    });
+    return offInit;
+  }, []);
+
+  return dbLoaded;
+};
+
+const App = () => {
+  const dbLoaded = useLoadDb();
+  if (!dbLoaded) return <LoadingOverlay />;
+  return (
+    <ToastContainer>
+      <RootRouter />
+    </ToastContainer>
+  );
+};
 
 export default App;
---
+++ src/db/collections.js
@@ -0,0 +1,3 @@
+export default (db) => {
+  db.addCollection('todos');
+};
