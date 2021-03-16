fb2a0315e0f4443e7a5713230ae8de5651bee1d0
erdghst79 <61949771+erdghst79@users.noreply.github.com>
2021-03-17 00:16:32 +0200

    T2. Highlight due dates in the todo list with different colors

--- src/components/todos/DueDate.js
+++ src/components/todos/DueDate.js
@@ -7,7 +7,26 @@ const DueDate = ({ date, format }) => {
     format,
   ]);
 
+if (date == undefined)
+  var i = new Date();
+else
+  var i = new Date(date);
+const DateFromNow = new Date();
+var date1 = Date.UTC(i.getFullYear(), i.getMonth(), i.getDate());
+var date2 = Date.UTC(DateFromNow.getFullYear(), DateFromNow.getMonth(), DateFromNow.getDate());
+var ms = date1-date2;
+var q =  Math.floor(ms/1000/60/60/24);
+
+
+
+if (q<1)
   return <span className="text-danger">{formatted}</span>;
+  else{
+  if (q<3 && q>0)
+   return <span className="text-warning">{formatted}</span>;
+  else {
+    return <span className="text-success">{formatted}</span>;
+  }}
 };
 
 export default DueDate;
